f885cc3f514566316d792fae31f126ad7af1ac13
tarikkeskin <tarik.keskin007@gmail.com>
2021-03-29 23:19:31 +0300

    create activities

--- app/src/main/java/com/tictactoe/kidsgame/MainActivity.java
+++ app/src/main/java/com/tictactoe/kidsgame/MainActivity.java
@@ -2,7 +2,9 @@ package com.tictactoe.kidsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goAnimalActivity(View view){
+
+        Intent intent = new Intent(this,AnimalActivity.class);
+
+        startActivity(intent);
+    }
+    public void goColorsActivity(View view){
+
+        Intent intent = new Intent(this,ColorActivity.class);
+
+        startActivity(intent);
+    }
+    public void goExercisesActivity(View view){
+
+        Intent intent = new Intent(this,ExercisesActivity.class);
+
+        startActivity(intent);
     }
 }
