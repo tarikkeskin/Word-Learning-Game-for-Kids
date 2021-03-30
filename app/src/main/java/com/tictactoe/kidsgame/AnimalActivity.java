11a8190673c6609e5fcf8084164dc2b88c73ef76
tarikkeskin <tarik.keskin007@gmail.com>
2021-03-30 22:14:49 +0300

    Genereta Random Animal Image implemantation

--- app/src/main/java/com/tictactoe/kidsgame/AnimalActivity.java
+++ app/src/main/java/com/tictactoe/kidsgame/AnimalActivity.java
@@ -2,13 +2,67 @@ package com.tictactoe.kidsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class AnimalActivity extends AppCompatActivity {
 
+
+
+
+
+    final Random rnd = new Random();
+
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_animal);
+
         setContentView(R.layout.activity_animal);
+
+        final ImageView img = (ImageView) findViewById(R.id.image_animal);
+        // I have 3 images named img_0 to img_2, so...
+        final String str = "imganimal_" + rnd.nextInt(2);
+        img.setImageDrawable
+                (getResources().getDrawable(getResourceID(str, "drawable", getApplicationContext())));
+
+        // globally
+        TextView myAwesomeTextView = (TextView) findViewById(R.id.textanimal);
+        //in your OnCreate() method
+        myAwesomeTextView.setText("My Awesome Text");
+
+    }
+
+    protected final static int getResourceID
+            (final String resName, final String resType, final Context ctx)
+    {
+        final int ResourceID =
+                ctx.getResources().getIdentifier(resName, resType,
+                        ctx.getApplicationInfo().packageName);
+        if (ResourceID == 0)
+        {
+            throw new IllegalArgumentException
+                    (
+                            "No resource string found with name " + resName
+                    );
+        }
+        else
+        {
+            return ResourceID;
+        }
+    }
+    public void goNextPicture(View view){
+
+        Intent intent = new Intent(this,AnimalActivity.class);
+
+        startActivity(intent);
     }
 }
